0a0888d459e19038264eebb006de899cff0fb322
Tony Chang <tchang@microsoft>
2019-02-18 08:33:41 -0800

    Initial commit of react demo

---
+++ gulpfile.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var gulp = require('gulp');
+var connect = require('gulp-connect');
+var open = require('gulp-open');
+var browserify = require('browserify');
+var reactify = require('reactify');
+var source = require('vinyl-source-stream');
+
+var config = {
+    port: 9005,
+    devBaseUrl: 'http://localhost',
+    paths: {
+        html: './src/*.html',
+        dist: './dist',
+        js: './src/**/*.js',
+        mainJs: './src/main.js'
+    }
+}
+
+gulp.task('connect', function(){
+    connect.server({
+        root: ['dist'],
+        port: config.port,
+        base: config.devBaseUrl,
+        livereload: true
+    })
+})
+
+gulp.task('open', ['connect'], function(){
+    gulp.src('dist/index.html')
+        .pipe(open({ uri: config.devBaseUrl + ':' + config.port + '/'}));
+});
+
+gulp.task('html', function() {
+    gulp.src(config.paths.html)
+    .pipe(gulp.dest(config.paths.dist))
+    .pipe(connect.reload());
+})
+
+gulp.task ('js', function() {
+    browserify(config.paths.mainJs)
+        .transform(reactify)
+        .bundle()
+        .on('error', console.error.bind(console))
+        .pipe(source('bundle.js'))
+        .pipe(gulp.dest(config.paths.dist + '/scripts'))
+        .pipe(connect.reload());
+})
+
+gulp.task('watch', function() {
+    gulp.watch(config.paths.html, ['html']);
+    gulp.watch(config.paths.js, ['js']);
+});
+
+gulp.task('default', ['html', 'js', 'open', 'watch']);
---
+++ src/main.js
@@ -0,0 +1,2 @@
+var App = console.log('Hello world from Browserify');
+module.exports = App;
