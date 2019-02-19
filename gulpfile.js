9169e7c670932483d3fe919ceb87100e37b6f8a9
Tony Chang <tchang@microsoft>
2019-02-18 16:14:45 -0800

    Adding bootstrap stuff

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,7 @@ var open = require('gulp-open');
 var browserify = require('browserify');
 var reactify = require('reactify');
 var source = require('vinyl-source-stream');
+var concat = require('gulp-concat'); 
 
 var config = {
     port: 9005,
@@ -14,6 +15,10 @@ var config = {
         html: './src/*.html',
         dist: './dist',
         js: './src/**/*.js',
+        css: [
+            'node_modules/bootstrap/dist/css/bootstrap.min.css',
+            'node_modules/bootstrap/dist/css/bootstrap-theme.min.css'
+        ],
         mainJs: './src/main.js'
     }
 }
@@ -48,9 +53,15 @@ gulp.task ('js', function() {
         .pipe(connect.reload());
 })
 
+gulp.task('css', function() {
+    gulp.src(config.paths.css)
+        .pipe(concat('bundle.css'))
+        .pipe(gulp.dest(config.paths.dist + '/css'));
+});
+
 gulp.task('watch', function() {
     gulp.watch(config.paths.html, ['html']);
     gulp.watch(config.paths.js, ['js']);
 });
 
-gulp.task('default', ['html', 'js', 'open', 'watch']);
+gulp.task('default', ['html', 'js', 'open', 'watch', 'css']);
